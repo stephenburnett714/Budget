191735866757eb1eb1191d6b71d70efa6ae02ff6
Alex Battle <home@Alex-Mac.local>
2020-02-18 17:05:18 -0500

    nav bars

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
+import ResultsNav from './components/ResultsNav';
+import SearchWidget from './components/SearchWidget';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <SearchWidget />
     </div>
   );
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReservationPage from './ReservationPage';
 import SearchWidget from './SearchWidget';
 import Nav from './Nav'
+import ResultsNav from './ResultsNav';
 
 function Main(){
     return(
         <div>
-            <Nav />
+            <ResultsNav />
         </div>
     )
 }
