90c677ef0f6622c3e1434da4bb59e08b98513967
Alex Battle <home@Alex-Mac.local>
2020-02-19 15:49:55 -0500

    final nav bars and results page

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,17 @@ import LandingPage from './components/landingPage'
 import { Route, withRouter } from 'react-router-dom'
 import SelectACar from './components/routes/SelectACar'
 import ResultsNav from './components/ResultsNav';
+import ResultsPage from './components/ResultsPage'
+
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/carselect" component={SelectACar} />
+        <ResultsNav/>
+        <ResultsPage/>
+        {/* <Route exact path="/" component={LandingPage} /> */}
+        {/* <Route exact path="/carselect" component={SelectACar} /> */}
       </div>
     </React.Fragment>
   );
